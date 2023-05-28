e8d5553d91073d9cb359deb0aa568fd3e8993afb
SimranAhuja00 <97106836+SimranAhuja00@users.noreply.github.com>
2023-05-28 23:22:35 +0530

    Create SwapTwoVarWithoutTemp.py
    Program to two variables without using the third variable in Python.

---
+++ NumberProgs/SwapTwoVarWithoutTemp.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the first number a: "))
+b = int(input("Enter the second number b: "))
+a=a-b
+b=a+b
+a=b-a
+print("After swapping")
+print("Value of a is : ", a);
+print("Value of b is : ", b); 
