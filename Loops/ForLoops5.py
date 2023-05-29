8cb4c515b9fe9e7929be1faecedbe2403a1f6ae4
Simran Ahuja <97106836+SimranAhuja00@users.noreply.github.com>
2023-05-30 02:05:37 +0530

    Create ForLoops5.py

---
+++ Loops/ForLoops5.py
@@ -0,0 +1,24 @@
+#iterating through a string 
+mystring = 'hello'
+
+for letter in mystring:
+    print(letter)
+
+# Output:-
+# h
+# e
+# l
+# l
+# o
+
+tup = (1,2,3)
+#iterating through a tuple
+
+for item in tup:
+    print(item)
+
+# Output:-
+# 1
+# 2
+# 3
+
