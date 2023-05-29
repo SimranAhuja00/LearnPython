e1a124374d693bdb3444a4fc594e2d55a064446c
Simran Ahuja <97106836+SimranAhuja00@users.noreply.github.com>
2023-05-30 02:18:09 +0530

    Create WhileLoop1.py

---
+++ Loops/WhileLoop1.py
@@ -0,0 +1,12 @@
+x = 0
+
+while x < 5:
+    print(f'The current value of x is {x}')
+    x = x + 1
+
+# Output:-
+# The current value of x is 0
+# The current value of x is 1
+# The current value of x is 2
+# The current value of x is 3
+# The current value of x is 4
