447b3276ba47f9254f65b83e025d789337680d0e
Simran Ahuja <97106836+SimranAhuja00@users.noreply.github.com>
2023-05-30 01:55:56 +0530

    Create ForLoops2.py

---
+++ Loops/ForLoops2.py
@@ -0,0 +1,14 @@
+mylist = [1,2,3,4,5,6,7,8,9,10]
+
+#to print only the even numbers present in the list
+for num in mylist:
+    #check for even
+    if num%2 == 0:
+        print(num)
+
+#Output:-
+# 2
+# 4
+# 6
+# 8
+# 10
