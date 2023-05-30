2e6346bacb22452227039f48e10565613e4b87fc
Simran Ahuja <97106836+SimranAhuja00@users.noreply.github.com>
2023-05-30 14:00:09 +0530

    Create RangeOperator.py
    different ways the range operator is used along with for loops

---
+++ Loops/RangeOperator.py
@@ -0,0 +1,28 @@
+for num in range(5):
+    print(num)
+
+# Output:-
+# 0
+# 1
+# 2
+# 3
+# 4
+
+for num in range(3,5):
+    print(num)
+
+# Output:-
+# 3
+# 4
+
+#Adding a step size
+
+for num in range(0,10,2):
+    print(num)
+
+# Output:-
+# 0
+# 2
+# 4
+# 6
+# 8
